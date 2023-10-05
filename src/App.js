c465d1291ff31dc2043a2936f6c6c38360ba9713
jana <veerajamail@gmail.com>
2023-10-04 21:09:27 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Welcome to student management system!
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
